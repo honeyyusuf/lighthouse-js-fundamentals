02982f3b8a27c424bae9bbcca289c40742b1b68e
honeyyusuf <yusufhon0@gmail.com>
2020-08-26 13:23:00 +0000

    output the array values

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for(var i = 0 ; i < ingredients.length; i++){
+  console.log (ingredients[i]);
+}
+let x = 0 ;
+while( x<ingredients.length ){
+  console.log (ingredients[x]);
+  x++;
+}
+for( var y = ingredients.length-1; y > -1 ; y--){
+  console.log (ingredients[y]);
+}
